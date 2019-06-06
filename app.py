db7829d5372f7765d8e891ffe3c2b676636bf0d3
nayorken <hugo.ferreira1617@gmail.com>
2019-06-06 19:00:35 +0100

    7

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ def existe(v1):
     ficheiro.close()
     return valor
 
-
 def eliminar(v1):
     import sqlite3
     ficheiro = sqlite3.connect('db/Utilizadores.db')
@@ -42,20 +41,16 @@ def eliminar(v1):
     db.execute("DELETE FROM usr WHERE usr = ? ", (v1,))
     ficheiro.commit()
     ficheiro.close()
-    return valor
-
-
-
+    return
 
 def log(v1, v2):
     import sqlite3
     ficheiro = sqlite3.connect('db/Utilizadores.db')
     db = ficheiro.cursor()
-    db.execute("SELECT * FROM usr WHERE usr = ? and pwd = ? ", (v1, v2,))
+    db.execute("SELECT * FROM usr WHERE usr = ? and pwd = ? ", (v1,v2,))
     valor = db.fetchone()
     ficheiro.close()
     return valor
-
 
 @app.route('/newpass', methods=['GET', 'POST'])
 def newpass():
@@ -89,7 +84,6 @@ def registo():
             erro = 'Utilizador  registado com sucesso'
     return render_template('registo.html', erro=erro)
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     erro = None
@@ -98,17 +92,11 @@ def login():
         v2 = request.form['pwd']
         if not existe(v1):
             erro = 'O utilizador não existe.'
-        elif not log(v1, v2):
+        elif not log(v1,v2):
             erro = 'A palavra passe incorreta.'
         else:
             erro = 'Bem Vindo!'
     return render_template('login.html', erro=erro)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
-
 
 @app.route('/apagar', methods=['GET', 'POST'])
 def apagar():
@@ -118,13 +106,16 @@ def apagar():
         v2 = request.form['pwd']
         if not existe(v1):
             erro = 'O utilizador não existe.'
-        elif not log(v1, v2):
+        elif not log(v1,v2):
             erro = 'A palavra passe incorreta.'
         else:
             eliminar(v1)
-            erro = 'Conta Eliminada Com Sucesso'
+            erro = 'Conta eliminada com sucesso'
     return render_template('apagar.html', erro=erro)
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
