577daa5501297889eb883d84a3a6b5c86d6b1134
hugoy <hugo.ferreira1617@gmail.com>
2019-06-11 16:34:24 +0100

    19

--- app.py
+++ app.py
@@ -26,13 +26,17 @@ def alterar(v1, v2):
 
 
 def existe(v1):
-    import sqlite3
-    ficheiro = sqlite3.connect('db/Utilizadores.db')
-    db = ficheiro.cursor()
-    db.execute("SELECT * FROM usr WHERE usr = ? ", (v1,))
-    valor = db.fetchone()
-    ficheiro.close()
+    try:
+        import sqlite3
+        ficheiro = sqlite3.connect('db/Utilizadores.db')
+        db = ficheiro.cursor()
+        db.execute("SELECT * FROM usr WHERE usr = ? ", (v1,))
+        valor = db.fetchone()
+        ficheiro.close()
+    except:
+        valor = None
     return valor
+
 
 def eliminar(v1):
     import sqlite3
@@ -43,14 +47,16 @@ def eliminar(v1):
     ficheiro.close()
     return
 
+
 def log(v1, v2):
     import sqlite3
     ficheiro = sqlite3.connect('db/Utilizadores.db')
     db = ficheiro.cursor()
-    db.execute("SELECT * FROM usr WHERE usr = ? and pwd = ? ", (v1,v2,))
+    db.execute("SELECT * FROM usr WHERE usr = ? and pwd = ? ", (v1, v2,))
     valor = db.fetchone()
     ficheiro.close()
     return valor
+
 
 @app.route('/newpass', methods=['GET', 'POST'])
 def newpass():
@@ -84,6 +90,7 @@ def registo():
             erro = 'Utilizador  registado com sucesso'
     return render_template('registo.html', erro=erro)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     erro = None
@@ -92,11 +99,12 @@ def login():
         v2 = request.form['pwd']
         if not existe(v1):
             erro = 'O utilizador não existe.'
-        elif not log(v1,v2):
+        elif not log(v1, v2):
             erro = 'A palavra passe incorreta.'
         else:
             erro = 'Bem Vindo!'
     return render_template('login.html', erro=erro)
+
 
 @app.route('/apagar', methods=['GET', 'POST'])
 def apagar():
@@ -106,16 +114,18 @@ def apagar():
         v2 = request.form['pwd']
         if not existe(v1):
             erro = 'O utilizador não existe.'
-        elif not log(v1,v2):
+        elif not log(v1, v2):
             erro = 'A palavra passe incorreta.'
         else:
             eliminar(v1)
             erro = 'Conta eliminada com sucesso'
     return render_template('apagar.html', erro=erro)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
